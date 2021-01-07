e8e952391180b54bc44d23099443051a078140d3
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2021-01-07 17:18:34 +0500

    Recursive Fluent Design pattern

---
+++ BuilderDesignPattern/FluentBuilderDesignPattern/EmployeeBuilderDirector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBuilderDesignPattern
+{
+    public class EmployeeBuilderDirector: EmployeeSalaryBuilder<EmployeeBuilderDirector>
+    {
+        public static EmployeeBuilderDirector NewEmployee => new EmployeeBuilderDirector();
+    }
+}
