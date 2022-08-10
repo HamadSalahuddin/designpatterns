93e1c4ca4c2cc6be2b6e321c985c77b9a980a2b8
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-10 18:09:11 +0500

    Implemented Factory design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/FactoryMethodDesignPattern/AirConditioner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDesignPattern
+{
+    public enum Actions
+    {
+        Cooling,
+        Warming
+    }
+
+    public class AirConditioner
+    {
+        private readonly Dictionary<Actions, AirConditionerFactory> _factories;
+
+        public AirConditioner()
+        {
+            _factories = new Dictionary<Actions, AirConditionerFactory>
+            {
+                { Actions.Cooling, new CoolingFactory() },
+                { Actions.Warming, new WarmingFactory() }
+            };
+        }
+
+        public IAirConditioner ExecuteCreation(Actions action, double temperature) => _factories[action].Create(temperature);
+    }
+}
---
+++ BuilderDesignPattern/BuilderDesignPattern/FactoryMethodDesignPattern/WarmingFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDesignPattern
+{
+    public class WarmingFactory : AirConditionerFactory
+    {
+        public override IAirConditioner Create(double temperature) => new WarmingManager(temperature);
+    }
+}
