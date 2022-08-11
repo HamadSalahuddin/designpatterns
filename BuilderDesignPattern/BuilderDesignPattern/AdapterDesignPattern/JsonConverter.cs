1580ce3960e9bb49bd8fa67a71e30af8baa1f937
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-11 15:23:12 +0500

    Implemented Adpater design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/AdapterDesignPattern/JsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern
+{
+    public class JsonConverter
+    {
+        private IEnumerable<Manufacturer> _manufacturers;
+
+        public JsonConverter(IEnumerable<Manufacturer> manufacturers)
+        {
+            _manufacturers = manufacturers;
+        }
+
+        public void ConvertToJson()
+        {
+            var jsonManufacturers = JsonSerializer.Serialize(_manufacturers, new JsonSerializerOptions { WriteIndented = true });
+
+            Console.WriteLine("\nPrinting JSON list\n");
+            Console.WriteLine(jsonManufacturers);
+        }
+    }
+}
---
+++ BuilderDesignPattern/BuilderDesignPattern/AdapterDesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlConverter = new XmlConverter();
+            var adapter = new XmlToJsonAdapter(xmlConverter);
+            adapter.ConvertXmlToJson();
+        }
+    }
+}
