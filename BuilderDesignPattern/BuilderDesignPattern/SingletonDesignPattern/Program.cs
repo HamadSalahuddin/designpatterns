e46c563c15ed3b6608f3a5c4094b460e2e1c6ec4
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-11 14:46:02 +0500

    Implemented Factory Design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+            var db4 = SingletonDataContainer.Instance;
+
+            Console.WriteLine($"db hascode {db.GetHashCode()}");
+            Console.WriteLine($"db2 hascode {db2.GetHashCode()}");
+            Console.WriteLine($"db3 hascode {db3.GetHashCode()}");
+            Console.WriteLine($"db4 hascode {db4.GetHashCode()}");
+        }
+    }
+}
