f61f4d838c64a77d141b7cdb919fdb80b6df3690
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-12 18:17:24 +0500

    Command Design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/CommandDesignPattern/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDesignPattern
+{
+    public enum PriceAction
+    {
+        Increase,
+        Decrease
+    }
+
+    public interface ICommand
+    {
+        void ExecuteAction();
+    }
+}
