c7590dcf59e51e21c9f6c65d2120e1ebee3f00a3
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-12 17:27:33 +0500

    Decorator Design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/DecoratorDesignPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DecoratorDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regularOrder = new RegularOrder();
+            Console.WriteLine(regularOrder.CalculateTotalOrderPrice());
+            Console.WriteLine();
+
+            var preOrder = new PreOrder();
+            Console.WriteLine(preOrder.CalculateTotalOrderPrice());
+            Console.WriteLine();
+
+            var premiumPreorder = new PremiumPreorder(preOrder);
+            Console.WriteLine(premiumPreorder.CalculateTotalOrderPrice());
+        }
+    }
+}
