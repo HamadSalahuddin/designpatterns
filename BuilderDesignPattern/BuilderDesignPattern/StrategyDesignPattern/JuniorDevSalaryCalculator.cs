fa6ac9d8c4e455e5bcc8a8663baa04e5ffaf3019
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2022-08-18 12:06:21 +0500

    Implemented Strategy design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/StrategyDesignPattern/JuniorDevSalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    public class JuniorDevSalaryCalculator: ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports)
+            =>
+                reports
+                    .Where(report => report.Level == DeveloperLevel.Junior)
+                    .Select(report => report.CalculateSalary())
+                    .Sum();
+    }
+}
---
+++ BuilderDesignPattern/BuilderDesignPattern/StrategyDesignPattern/SeniorDevSalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    public class SeniorDevSalaryCalculator: ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports)
+            =>
+                reports
+                    .Where(report => report.Level == DeveloperLevel.Senior)
+                    .Select(report => report.CalculateSalary() * 1.2)
+                    .Sum();
+    }
+}
