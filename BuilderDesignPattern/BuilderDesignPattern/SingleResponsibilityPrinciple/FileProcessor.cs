b2fe7af3e74465bab5ee03ced67f83a71afe1298
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2023-07-31 12:39:53 +0500

    Single Responsibility principle

---
+++ BuilderDesignPattern/BuilderDesignPattern/SingleResponsibilityPrinciple/FileProcessor.cs
@@ -0,0 +1,28 @@
+﻿namespace SingleResponsibilityPrinciple
+{
+    public class FileProcessor
+    {
+        private readonly string fullFilePath;
+        public FileProcessor(string fullFilePath)
+        {
+            this.fullFilePath = fullFilePath;
+        }
+
+        public string ReadAllText()
+        {
+            return System.Web.HttpUtility.HtmlEncode(File.ReadAllText(fullFilePath));
+        }
+
+        public void WriteToFile(string text)
+        {
+            var outputFilePath =
+               Path.GetDirectoryName(fullFilePath) +
+               Path.DirectorySeparatorChar +
+               Path.GetFileNameWithoutExtension(fullFilePath) +
+               ".html";
+
+            using StreamWriter file = new(outputFilePath);
+            file.Write(text);
+        }
+    }
+}
