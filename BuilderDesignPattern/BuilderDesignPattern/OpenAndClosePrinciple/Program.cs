9b33100131f262d8f0eb966ad67f4b30e39dc9d5
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2023-08-01 16:51:23 +0500

    OpenAndClosed Principle

---
+++ BuilderDesignPattern/BuilderDesignPattern/OpenAndClosePrinciple/Program.cs
@@ -0,0 +1,96 @@
+﻿using OpenAndClosedPrinciple;
+using SingleResponsibilityPrinciple;
+
+
+/*try
+{
+    Console.WriteLine("Please specify the file to convert to HTML.");
+    var fullFilePath = Console.ReadLine() ?? string.Empty;
+    var fileProcessor = new FileProcessor(fullFilePath);
+
+    var tagsToReplace = new Dictionary<string, (string, string)>
+    {
+        { "**", ("<strong>", "</strong>") },
+        { "*", ("<em>", "</em>") },
+        { "~~", ("<del>", "</del>") }
+    };
+
+    var textProcessor = new MdTextProcessor(tagsToReplace);
+    var inputText = fileProcessor.ReadAllText();
+    var outputText = textProcessor.ConvertText(inputText);
+    fileProcessor.WriteToFile(outputText);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();*/
+
+AreaCalculator calculator = new AreaCalculator();
+
+Square square = new Square { Side = 5 };
+Circle circle = new Circle { Radius = 3 };
+Triangle triangle = new Triangle { Base = 4, Height = 3 };
+
+Shape[] shapes = { square, circle, triangle };
+
+double totalArea = calculator.CalculateTotalArea(shapes);
+Console.WriteLine($"Total Area: {totalArea}");
+
+
+public class Shape
+{
+    public virtual double Area()
+    {
+        return 0;
+    }
+}
+
+public class Square : Shape
+{
+    public double Side { get; set; }
+
+    public override double Area()
+    {
+        return Side * Side;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double Area()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Triangle : Shape
+{
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    // Incorrect override!
+    public override double Area()
+    {
+        return (Base * Height) / 2;
+    }
+}
+
+
+
+public class AreaCalculator
+{
+    public double CalculateTotalArea(Shape[] shapes)
+    {
+        double totalArea = 0;
+        foreach (Shape shape in shapes)
+        {
+            totalArea += shape.Area();
+        }
+        return totalArea;
+    }
+}
+
---
+++ BuilderDesignPattern/BuilderDesignPattern/OpenAndClosePrinciple/TextProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class TextProcessor
+    {
+        public virtual string ConvertText(string inputText)
+        {
+            var paragraphs = Regex
+               .Split(inputText, @"(\r\n?|\n)")
+               .Where(p => p.Any(char.IsLetterOrDigit));
+
+            var sb = new StringBuilder();
+
+            foreach (var paragraph in paragraphs)
+            {
+                if (paragraph.Length == 0)
+                    continue;
+
+                sb.AppendLine($"<p>{paragraph}</p>");
+            }
+
+            sb.AppendLine("<br/>");
+            return sb.ToString();
+        }
+    }
+}
