dafba96d140528d30d79c016f8d19bcf00dc6c5c
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2023-11-07 17:51:00 +0500

    SRP

---
+++ BuilderDesignPattern/BuilderDesignPattern/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SingleResponsibilityPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please specify the file to convert to HTML.");
+                var fullFilePath = Console.ReadLine();
+                var fileProcessor = new FileProcessor(fullFilePath!);
+                var textProcessor = new TextProcessor(fileProcessor);
+                textProcessor.ConvertText();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
