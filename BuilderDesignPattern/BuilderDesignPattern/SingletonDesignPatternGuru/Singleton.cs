51332c1756e86f45c72010d15ea4c9ea135a1018
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2023-12-18 16:24:53 +0500

    Singleton design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/SingletonDesignPatternGuru/Singleton.cs
@@ -0,0 +1,66 @@
+﻿namespace SingletonDesignPatternGuru
+{
+    // The Singleton class defines the `GetInstance` method that serves as an
+    // alternative to constructor and lets clients access the same instance of
+    // this class over and over.
+
+    // EN : The Singleton should always be a 'sealed' class to prevent class
+    // inheritance through external classes and also through nested classes.
+    public sealed class Singleton
+    {
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton() { }
+
+        // The Singleton's instance is stored in a static field. There there are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static Singleton _instance;
+
+        // We now have a lock object that will be used to synchronize threads
+        // during first access to the Singleton.
+        private static readonly object _lock = new object();
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                // Now, imagine that the program has just been launched. Since
+                // there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait here.
+                lock (_lock)
+                {
+                    // The first thread to acquire the lock, reaches this
+                    // conditional, goes inside and creates the Singleton
+                    // instance. Once it leaves the lock block, a thread that
+                    // might have been waiting for the lock release may then
+                    // enter this section. But since the Singleton field is
+                    // already initialized, the thread won't create a new
+                    // object.
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+
+        public string Value { get; set; }
+    }
+}
