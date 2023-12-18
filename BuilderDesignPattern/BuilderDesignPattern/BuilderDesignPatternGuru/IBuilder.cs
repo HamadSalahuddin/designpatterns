320bb3e873d7f3dd35c2643c217728f46569aecf
Hamad Salahuddin <43289397+HamadSalahuddin@users.noreply.github.com>
2023-12-18 13:00:58 +0500

    Builder design pattern

---
+++ BuilderDesignPattern/BuilderDesignPattern/BuilderDesignPatternGuru/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace BuilderDesignPatternGuru
+{
+    // The Builder interface specifies methods for creating the different parts
+    // of the Product objects.
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+}
